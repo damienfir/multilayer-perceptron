4c4cc5f9d2681995b3c8d0edba096f0c80269628
Nicolas Voirol <nicolas.voirol@epfl.ch>
2013-04-24 23:39:05 +0200

    solved verification for pure online mode

--- run.py
+++ run.py
@@ -6,46 +6,31 @@ import mlp, lstsq, logistic
 path = 'norb/processed_binary.mat'
 keys = ('training_left', 'training_right', 'training_cat')
 stream = streamer.Stream(path, keys, count=1)
-mlp_classifier = mlp.MLP(0.01, 0.1, 5, 5)
+mlp_classifier = mlp.MLP(0.01, 0.1, 10, 10)
 lstsq_classifier = lstsq.LeastSquares(0.1)
 log_classifier = logistic.LogisticLoss(0.01, 0.1)
 
-def verify_mlp_gradients(inputs, count):
-	sys.stdout.write("Verifying gradient computation ... ")
-	sys.stdout.flush()
-	all_errors = []
-	for _ in xrange(0, inputs):
-		x_left, x_right, t = stream.next()
-		errors = mlp_classifier.verify(x_left, x_right, t, count=count)
-		all_errors.extend(errors)
-	print "[DONE]"
-	if all_errors:
-		print "Errors found:"
-		for idx, x, y, mlp_result, result in all_errors:
-			print "ws[" + str(idx) + "][" + str(x) + "," + str(y) + "] ->",
-			print "mlp gradient of " + str(mlp_result) + " vs directional of " + str(result)
-		print "Total:", len(all_errors)
-	else:
-		print "No errors uncovered after " + str(count) + " random directions in " + str(inputs) + " inputs"
+def classify(classifier):
+	x_left, x_right, t = stream.next()
+	result = classifier.classify(x_left, x_right)
+	print "Result =?"
+	print np.vstack([t, result])
 
-def train_mlp():
+def test_mlp():
 	for _ in xrange(0, 100):
 		x_left, x_right, t = stream.next()
 		mlp_classifier.train(x_left, x_right, t)
+	classify(mlp_classifier)
 
-def train_least_squares():
+def test_least_squares():
 	x_left, x_right, t = stream.all()
 	lstsq_classifier.train(x_left, x_right, t)
+	classify(lstsq_classifier)
 
-def train_logistic():
+def test_logistic():
 	for _ in xrange(0, 100):
 		x_left, x_right, t = stream.next()
 		log_classifier.train(x_left, x_right, t)
+	classify(log_classifier)
 
-
-verify_mlp_gradients(100, 100)
-#train_logistic()
-#x_left, x_right, t = stream.next()
-#result = log_classifier.classify(x_left, x_right)
-#print "Result =?"
-#print np.vstack([t, result])
+test_logistic()
