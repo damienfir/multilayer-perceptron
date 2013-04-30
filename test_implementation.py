28e90814d88da2b14562354b32ee283db0bceb7f
Nicolas Voirol <nicolas.voirol@epfl.ch>
2013-04-30 11:24:02 +0200

    added testing stream generation

--- test_implementation.py
+++ test_implementation.py
@@ -1,5 +1,7 @@
 import sys
+import scipy.io
 import numpy as np
+from utils import *
 import streamer
 import mlp, lstsq, logistic
 
@@ -8,25 +10,46 @@ mlp_classifier = mlp.MLP(0.01, 0.1, 10, 10)
 lstsq_classifier = lstsq.LeastSquares(0.1)
 log_classifier = logistic.LogisticLoss(0.01, 0.1)
 
-def stream_binary():
+def training_binary(count=block_size):
 	path = 'norb/processed_binary.mat'
 	keys = ('training_left', 'training_right', 'training_cat')
-	return streamer.Stream(path, keys, count=block_size)
+	return streamer.Stream(path, keys, count=count)
 
-def validations_binary():
+def validations_binary(count=1):
 	path = 'norb/processed_binary.mat'
 	keys = ('validation_left', 'validation_right', 'validation_cat')
-	return streamer.Stream(path, keys)
+	return streamer.Stream(path, keys, count=count)
 
-def stream_5class():
+def training_5class(count=block_size):
 	path = 'norb/processed_5class.mat'
 	keys = ('training_left', 'training_right', 'training_cat')
-	return streamer.Stream(path, keys, count=block_size)
+	return streamer.Stream(path, keys, count=count)
 
-def validations_5class():
+def validations_5class(count=1):
 	path = 'norb/processed_5class.mat'
 	keys = ('validation_left', 'validation_right', 'validation_cat')
-	return streamer.Stream(path, keys)
+	return streamer.Stream(path, keys, count=count)
+
+def normalized_testing_stream(path, params_path, count):
+	data = scipy.io.loadmat(params_path)
+	mean_left, sigma_left = map(lambda x: np.tile(np.mat(x).T, (x.shape[0], count)), data['params_left'])
+	mean_right, sigma_right = map(lambda x: np.tile(np.mat(x).T, (x.shape[0], count)), data['params_right'])
+	def data_map(key, x):
+		if key == 'test_left_s':    return m(1.0 / sigma_left, x - mean_left)
+		elif key == 'test_right_s': return m(1.0 / sigma_right, x - mean_right)
+		else:                       return x
+	keys = ('test_left_s', 'test_right_s', 'test_cat_s')
+	return streamer.Stream(path, keys, count=count, map=data_map)
+
+def testing_binary(count=1):
+	path = 'norb/norb_binary.mat'
+	params_path = 'norb/processed_binary.mat'
+	return normalized_testing_stream(path, params_path, count)
+
+def testing_5class(count=1):
+	path = 'norb/norb_5class.mat'
+	params_path = 'norb/processed_5class.mat'
+	return normalized_testing_stream(path, params_path, count)
 
 def classify(classifier, validations, equals):
 	errors = 0.0
@@ -47,20 +70,20 @@ def classify_5class(classifier):
 	return classify(classifier, validations_5class(), lambda t,res: t == res)
 
 def test_mlp_binary():
-	stream = stream_binary()
+	stream = training_binary()
 	for _ in xrange(0, 100):
 		x_left, x_right, t = stream.next()
 		mlp_classifier.train(x_left, x_right, t)
 	classify_binary(mlp_classifier)
 
 def test_least_squares():
-	stream = stream_5class()
+	stream = training_5class()
 	x_left, x_right, t = stream.all()
 	lstsq_classifier.train(x_left, x_right, t)
 	classify_5class(lstsq_classifier)
 
 def test_logistic():
-	stream = stream_5class()
+	stream = training_5class()
 	for _ in xrange(0, 100):
 		x_left, x_right, t = stream.next()
 		log_classifier.train(x_left, x_right, t)
